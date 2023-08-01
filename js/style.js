43d9fb3acae541f663a228076a0627cbaab60b52
andrey <pop4666@yandex.ru>
2023-08-01 20:44:51 +0300

    test7

--- js/style.js
+++ js/style.js
@@ -19,7 +19,14 @@ productsButton.forEach((event)=>{
 
 const showMenu = document.querySelector('.show_menu');
 const btnBurgerMenu = document.querySelector('.btn_burger_menu')
+const mobileNav= document.querySelector('.mobile_nav ')
 
 btnBurgerMenu.addEventListener('click', () => {
     showMenu.classList.toggle('show_menu');
+    mobileNav.style='transition: height 1s liner 250ms';
+
+
+
+
+
 });
