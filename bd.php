239565614c1b411f49177be3f98092e8d9936236
andrey <pop4666@yandex.ru>
2023-08-12 18:19:15 +0300

    test11

---
+++ bd.php
@@ -0,0 +1,25 @@
+<?php
+$servername = "82.202.174.35";
+$username = "user113704111";
+$password = "Dronrus1234";
+
+$conn = new mysqli ($servername, $username, $password);
+
+if($conn->connect_error){
+   die ("ok:" .$conn->connect_error);
+   }
+   echo "no"
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+</body>
+</html>
